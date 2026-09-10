d283e9b7e420b9655241120cfdd5faa54a453450
jacobkatzen <jakatzen@icloud.com>
2026-09-10 14:57:12 -0400

    hello world

---
+++ week2_JacobKatzen.py
@@ -0,0 +1,12 @@
+variable1 = 10
+variable2 = 15
+
+addition = variable1+variable2
+subtraction = variable1-variable2
+multiplication = variable1*variable2
+division = variable2/variable1
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
