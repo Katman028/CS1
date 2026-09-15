5a3c2ba638f0584296c925ea910fd2db70e9150e
jacobkatzen <jakatzen@icloud.com>
2026-09-15 14:56:52 -0400

    hello world

---
+++ Project1_JacobKatzen.py
@@ -0,0 +1,57 @@
+name = "Jacob Katzen"
+
+price1 = 5.00
+price2 = 7.50
+price3 = 20.99
+price4 = 15.50
+price5 = 3.20
+
+amount1 = 5
+amount2 = 2
+amount3 = 7
+amount4 = 3
+amount5 = 4
+total_amount = amount1 + amount2 + amount3 + amount4 + amount5
+
+product1 = price1*amount1
+product2 = price2*amount2
+product3 = price3*amount3
+product4 = price4*amount4
+product5 = price5*amount5
+
+total = product1 + product2 + product3 + product4 + product5
+
+average = total/total_amount
+
+
+
+print(f"{name.title()}: Total bill ${round(total,2)}, average price per item ${round(average,2)}")
+
+
+name = "Jacob Katzen"
+
+price1 = 6.00
+price2 = 9.50
+price3 = 11.99
+price4 = 12.50
+price5 = 14.20
+
+amount1 = 3
+amount2 = 7
+amount3 = 2
+amount4 = 6
+amount5 = 2
+total_amount = amount1 + amount2 + amount3 + amount4 + amount5
+
+product1 = price1*amount1
+product2 = price2*amount2
+product3 = price3*amount3
+product4 = price4*amount4
+product5 = price5*amount5
+
+total = product1 + product2 + product3 + product4 + product5
+average = total/total_amount
+
+
+
+print(f"{name.title()}: Total bill ${round(total,2)}, average price per item ${round(average,2)}")
