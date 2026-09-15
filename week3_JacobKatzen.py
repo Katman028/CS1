0f729a1db59a06b785150f9f34bb21326dc4f2f8
Jacob Katzen <jakatzen@icloud.com>
2026-09-15 15:02:18 -0400

    Add print statements for questions in week3_JacobKatzen.py

--- week3_JacobKatzen.py
+++ week3_JacobKatzen.py
@@ -6,7 +6,7 @@ rating = 5
 HIGHEST_RATING = 5
 #multiple assignment for variables
 favorite_actor,favorite_character = ("Harrison Ford", "Han Solo")
-
+print("question1")
 print("my movie")
 print("--------")
 print(f" movie: {movie_name}")
@@ -21,11 +21,11 @@ print(f" favorite character: {favorite_character}")
 people_in_class =  25
 groups_of_3 = people_in_class//3
 amount_left_over = people_in_class%3
-
+print("question2")
 print(f" there is {people_in_class} people class, there can be {groups_of_3} groups of 3, with {amount_left_over} people left")
 
 
 farenheit = 75
 farenheit_to_celsius = (farenheit-32)*5/9
-
+print("question3")
 print(f" the weather is {farenheit} degrees farenheit and {farenheit_to_celsius} degrees celsius")
