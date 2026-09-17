ed386fc956f74d9497caf8a4b326c43e03126790
jacobkatzen <jakatzen@icloud.com>
2026-09-17 14:37:22 -0400

    COMP-151 week3b_jacobKatzen.py

---
+++ Week3b_JacobKatzen.py
@@ -0,0 +1,23 @@
+coffee_per_week = 5
+cost_per_cup = 3.50
+cost_per_week = coffee_per_week * cost_per_cup
+
+print(f"total amount spent per week {cost_per_week}")
+
+name = "Jacob"
+print(f"Helo, {name} what is your favorite color")
+
+name = "Jacob Katzen"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+print("Yoda said 'Do or do not there is no try.'")
+
+famous_person = "Yoda"
+famous_quote = "'Do or do not there is no try.'"
+print(f"{famous_person}, said  {famous_quote}")
+
+name = "   Jacob katzen   "
+
+print(f"{name.rstrip()}\n{name.lstrip()}")
